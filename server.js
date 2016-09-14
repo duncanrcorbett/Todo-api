2e8ec0e0008c1e405a44fcbc5988f753ed9975eb
Duncan Corbett <duncanrcorbett@gmail.com>
2016-09-14 19:05:40 +0200

    Refactor API with underScore

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var _ = require('underscore');
 
 var app = express();
 var PORT = process.env.PORT || 3000;
@@ -8,46 +9,62 @@ var todoNextId = 1;
 
 app.use(bodyParser.json());
 
-app.get('/', function(req, res){
-   res.send('Todo API Root');
+app.get('/', function (req, res) {
+    res.send('Todo API Root');
 });
 
-//GET /todos
-app.get('/todos',function (req,res){
-   res.json(todos);
+// GET /todos
+app.get('/todos', function (req, res) {
+    res.json(todos);
 });
-//GET /todo/:id
-app.get('/todos/:id', function(req,res){
-   var todoId = parseInt(req.params.id, 10);
-   var matched;
 
-   // itterate over todos array looking for match
-   todos.forEach(function (todo){
-      if (todoId === todo.id){
-         matched = todo;
-      }
-   });
+// GET /todos/:id
+app.get('/todos/:id', function (req, res) {
+    var todoId = parseInt(req.params.id, 10);
+    var matchedTodo = _.findWhere(todos, {id: todoId});
 
-   if (matched){
-      res.json(matched);
-   } else {
-      res.status('404').send();
-   }
-
+    if (matchedTodo) {
+        res.json(matchedTodo);
+    } else {
+        res.status(404).send();
+    }
 });
 
 // POST /todos
-app.post('/todos', function (req, res){
-var body = req.body;
+app.post('/todos', function (req, res) {
+    var body = _.pick(req.body, 'description', 'completed');
 
-    // add ID field
+    if (!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length === 0) {
+        return res.status(400).send();
+    }
+
+    body.description = body.description.trim();
     body.id = todoNextId++;
+
     todos.push(body);
-    // push body into array
 
     res.json(body);
 });
 
-app.listen(PORT, function (){
-   console.log('Express listening on Port: ' + PORT + '!');
+
+app.listen(PORT, function () {
+    console.log('Express listening on port ' + PORT + '!');
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
