05f13305f1dc2669a8c499b630c26bb6d5203d97
Duncan Corbett <duncanrcorbett@gmail.com>
2016-09-14 22:30:53 +0200

    Added ?completed filter

---
+++ variable-playground.js
@@ -0,0 +1,24 @@
+// var person = {
+//     name: 'Duncan',
+//     age: 21
+// };
+//
+// function updatePerson (obj){
+//     obj.age = 24
+// };
+//
+// updatePerson(person);
+// console.log(person);
+
+//array example
+
+var grades = [50,70];
+
+function updateGrades (gradesArr){
+    gradesArr.push(55);
+  //  debugger;
+  // gradesArr = [12,44,99]
+}
+
+updateGrades(grades);
+console.log(grades);
