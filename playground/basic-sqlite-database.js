b1acd2ebfaf7279aecd20ce056a8ed86b0dec8bd
Duncan Corbett <duncanrcorbett@gmail.com>
2016-09-22 21:49:22 +0200

    added Todo user assosiation

--- playground/basic-sqlite-database.js
+++ playground/basic-sqlite-database.js
@@ -20,46 +20,38 @@ var Todo = sequelize.define('todo', {
     }
 });
 
+var User = sequelize.define('user', {
+    email: Sequelize.STRING
+});
+
+Todo.belongsTo(User);
+User.hasMany(Todo);
+
 sequelize.sync({
     //force: true
 }).then(function (){
     console.log('Everything is Synced');
 
-    Todo.findById(2).then(function (todo){
-        if (todo){
-            console.log(todo.toJSON)
-        } else{
-            console.log('Todo Not found');
-        }
+    User.findById(1).then(function (user){
+        user.getTodos({
+            where :{
+                completed: false
+            }
+        }).then(function (todos){
+            todos.forEach(function (todo){
+                console.log(todo.toJSON());
+            })
+        })
     });
-
-    // Todo.create({
-    //     description: "walking the trash",
-    //     completed: false
-    // }).then( function(todo){
-    //    return Todo.create({
-    //        description: "Go to the shops"
-    //    });
-    // }).then(function(){
-    //    // return Todo.findById(1)
-    //     return Todo.findAll({
-    //         where: {
-    //             description: {
-    //                 $like: '%trash%'
-    //             }
-    //         }
-    //     })
-    //         .then (function (todos) {
-    //             if (todos){
-    //                 todos.forEach(function (todo) {
-    //                     console.log(todo.toJSON())
-    //                 });
-    //
-    //             } else {
-    //                 console.log('no todo Found')
-    //             }
-    //         })
-    // }).catch(function (e){
-    //     console.log(e);
-    //});
+    // User.create({
+    //     email: 'duncan@duncan.com'
+    // }).then(function () {
+    //     return Todo.create({
+    //         description: 'clean yard'
+    //     });
+    // }).then(function (todo){
+    //     User.findById(1).then(function (user){
+    //         user.addTodo(todo);
+    //     });
+    // });
 });
